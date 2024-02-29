80a7342fd9fb6cba6025cfe19505b7aa4ab0b3d2
andiigela <andiigela@gmail.com>
2024-02-29 21:54:00 +0100

    Task 1: Count the number of words in the file.

--- rdds/task1.py
+++ rdds/task1.py
@@ -4,4 +4,7 @@ conf = SparkConf().setAppName("Spark Tasks")
 sc = SparkContext.getOrCreate(conf=conf);
 
 rdd = sc.textFile(r"C:\Users\andig\PycharmProjects\PySpark-RDD-Tasks\rdds\input_files\Second Test.txt")
-print(rdd.collect())
+
+# Task 1: Count the number of words in the file.
+rdd2 = rdd.flatMap(lambda x: x.split());
+print(rdd2.count())
