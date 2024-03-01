1329530979b6f7924c840838257767bd39410845
andiigela <andiigela@gmail.com>
2024-03-01 17:24:08 +0100

    Task 7: Show the total marks achieved by Female and Male students

--- rdds/student-tasks.py
+++ rdds/student-tasks.py
@@ -8,4 +8,8 @@ headers = rdd.first();
 
 # Task 6: Show the number of students in the file
 rdd2 = rdd.filter(lambda x: x != headers)
-rdd2.count()
+print(rdd2.count())
+
+# Task 7: Show the total marks achieved by Female and Male students
+rdd3 = rdd.filter(lambda x: x != headers).map(lambda x: (x.split(",")[1],int(x.split(",")[5]))).reduceByKey(lambda x,y: x+y)
+print(rdd3.collect())
