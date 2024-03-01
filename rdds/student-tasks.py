eb08759b5e27ed5ebc1a0b081889c9a2c9a87e39
andiigela <andiigela@gmail.com>
2024-03-01 17:25:51 +0100

    Task 8: Show the total number of students that have passed and failed. 50+ marks are required to pass the course

--- rdds/student-tasks.py
+++ rdds/student-tasks.py
@@ -13,3 +13,10 @@ print(rdd2.count())
 # Task 7: Show the total marks achieved by Female and Male students
 rdd3 = rdd.filter(lambda x: x != headers).map(lambda x: (x.split(",")[1],int(x.split(",")[5]))).reduceByKey(lambda x,y: x+y)
 print(rdd3.collect())
+
+# Task 8: Show the total number of students that have passed and failed. 50+ marks are required to pass the course
+rdd4 = rdd.filter(lambda x: x != headers);
+passed = rdd4.filter(lambda x: int(x.split(",")[5]) > 50).count();
+failed = rdd4.filter(lambda x: int(x.split(",")[5]) <= 50).count();
+print(f"Passed {passed}")
+print(f"Failed {failed}")
