ff29e786bc6576a2fe06c2e5e2114ac6cb41a6a6
andiigela <andiigela@gmail.com>
2024-03-04 22:34:53 +0100

    Task 15: Create a new column average to calc the average marks of the student

--- rdds/spark_dff1.py
+++ rdds/spark_dff1.py
@@ -7,3 +7,6 @@ df = spark.read.csv(r"C:\Users\andig\PycharmProjects\PySpark-RDD-Tasks\rdds\inpu
 # Task 14: Create a new column in the DF for total marks and let the total marks be 120
 df = df.withColumn("total marks", lit(120))
 
+# Task 15: Create a new column average to calc the average marks of the student (marks/total marks)*100
+df = df2.withColumn("average",(col("marks") / col("total marks"))*100)
+
