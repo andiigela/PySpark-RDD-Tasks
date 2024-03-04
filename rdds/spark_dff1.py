5f8fb699c3fe1a1b6fd5d4f6c10d448cd40b56c5
andiigela <andiigela@gmail.com>
2024-03-04 22:36:07 +0100

    Task 16: Filter out all students who have achieved more than 80% marks in OOP course and save it in new DF.

--- rdds/spark_dff1.py
+++ rdds/spark_dff1.py
@@ -9,4 +9,8 @@ df = df.withColumn("total marks", lit(120))
 
 # Task 15: Create a new column average to calc the average marks of the student (marks/total marks)*100
 df = df2.withColumn("average",(col("marks") / col("total marks"))*100)
+print(df.show())
 
+# Task 16: Filter out all students who have achieved more than 80% marks in OOP course and save it in new DF.
+df2 = df.filter((df.average > 80.0) & (df.course == "OOP") )
+print(df2.show())
