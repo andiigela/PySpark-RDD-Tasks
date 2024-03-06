f08938082fee2f3adea4d3bbad041c22de006ec0
andiigela <andiigela@gmail.com>
2024-03-06 18:54:18 +0100

    Task 27: Print the total numbers of employees in the company

--- rdds/dataframe tasks/spark_dff2.py
+++ rdds/dataframe tasks/spark_dff2.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import avg, count, min, max, sum
+
 spark = SparkSession.builder.appName("Quiz Dff2").getOrCreate()
 df = spark.read.csv("/FileStore/tables/StudentData.csv",header=True,inferSchema=True);
 
@@ -11,6 +12,7 @@ print(df.groupBy(df.course,df.gender).agg(count("*")).show());
 
 # Task 25: Display the total marks achieved by each gender in each course
 print(df.groupBy(df.gender,df.course).sum("marks").show());
+
 
 # Task 26: Display the minimum, maximum and average marks achieved in each course by each age group
 print(df.groupBy(df.course,df.age).agg(min("marks").alias("min_marks"),max("marks").alias("max_marks"),avg("marks").alias("avg_marks")).show())
---
+++ rdds/dataframe tasks/spark_dff3.py
@@ -0,0 +1,8 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count,sum,avg,min, max,col
+
+spark = SparkSession.builder.appName("Fifth Quiz").getOrCreate();
+df = spark.read.csv(r"C:\Users\andig\PycharmProjects\PySpark-RDD-Tasks\rdds\input_files\OfficeDataProject.csv",header=True,inferSchema=True);
+
+#Task 27: Print the total numbers of employees in the company
+print(df.dropDuplicates(["employee_id"]).count()) # or df.count();
