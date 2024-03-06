1197c5efe6a02bdb7ae5ea151732b46313cf9340
andiigela <andiigela@gmail.com>
2024-03-06 18:56:30 +0100

    Task 30: Print the total number of employees in each department

--- rdds/dataframe tasks/spark_dff3.py
+++ rdds/dataframe tasks/spark_dff3.py
@@ -4,11 +4,14 @@ from pyspark.sql.functions import count,sum,avg,min, max,col
 spark = SparkSession.builder.appName("Fifth Quiz").getOrCreate();
 df = spark.read.csv(r"C:\Users\andig\PycharmProjects\PySpark-RDD-Tasks\rdds\input_files\OfficeDataProject.csv",header=True,inferSchema=True);
 
-#Task 27: Print the total numbers of employees in the company
+# Task 27: Print the total numbers of employees in the company
 print(df.dropDuplicates(["employee_id"]).count()) # or df.count();
 
-#Task 28: Print the total numbers of departments in the company
+# Task 28: Print the total numbers of departments in the company
 print(df.select("department").distinct().count()) # or df.dropDuplicates(["department"]).count()
 
-#Task 29: Print the departments name of the company
-print(df.select("department").distinct().show())
+# Task 29: Print the departments name of the company
+print(df.select("department").distinct().show());
+
+# Task 30: Print the total number of employees in each department
+print(df.groupBy(["department"]).agg(count("*")).show());
